f19c920e131a2c9cd71a548772b67d4000a59877
鄭筑予 <154043164+AnnaCheng58796907@users.noreply.github.com>
2024-11-23 11:51:33 +0800

    lesson4_4

--- lesson4/lesson4_1.py
+++ lesson4/lesson4_1.py
@@ -1,5 +1,5 @@
 # 20241123 BMI計算更新進階
-def get_status(bmi:float)->str:
+def get_status(bmi: float) -> str:
     '''
     docstring
     Parameter:
@@ -12,29 +12,29 @@ def get_status(bmi:float)->str:
         - 中度肥胖
         - 重度肥胖
     '''
-    if bmi >= 35 :
+    if bmi >= 35:
         bmi_str = '重度肥胖'
-    elif bmi >= 30 :
+    elif bmi >= 30:
         bmi_str = '中度肥胖'
-    elif bmi >= 27 :
+    elif bmi >= 27:
         bmi_str = '輕度肥胖'
-    elif bmi >= 24 :
+    elif bmi >= 24:
         bmi_str = '過重'
-    elif bmi >= 18.5 :
+    elif bmi >= 18.5:
         bmi_str = '正常範圍'
-    else :
-        bmi_str = '過輕' 
+    else:
+        bmi_str = '過輕'
     return bmi_str
 
 
-def BMI_math(height_cm:float, weight_kg:float)->tuple[float, str]:
+def BMI_math(height_cm: float, weight_kg: float) -> tuple[float, str]:
     height_m = round(height_cm/100, 2)
     bmi_kg_m2 = round(weight_kg/(height_m**2), 2)
     bmi_str = get_status(bmi_kg_m2)
     return bmi_kg_m2, bmi_str
 
 
-while(True):
+while (True):
     try:
         height_cm = float(input("請輸入身高(公分):"))
         weight_kg = float(input("請輸入體重(公斤):"))
@@ -42,6 +42,6 @@ while(True):
         break
     except Exception:
         print('輸入格式錯誤,請重新輸入!')
-        
+
 print(f"您的BMI值是{bmi_value}\n您的體重{bmi_str}")
 print("程式結束")
--- lesson4/tools.py
+++ lesson4/tools.py
@@ -1,32 +1,28 @@
-def get_status(bmi:float)->str:
-    '''
-    docstring
-    Parameter:
-        bmi:bmi值,可以整數或浮點數
-    Return:str
-        會傳出五個狀態
-        - 正常範圍
-        - 過重
-        - 輕度肥胖
-        - 中度肥胖
-        - 重度肥胖
-    '''
-    if bmi >= 35 :
+PI = 3.1415962
+
+
+class Person:
+    pass
+
+
+def get_status(bmi: float) -> str:
+
+    if bmi >= 35:
         bmi_str = '重度肥胖'
-    elif bmi >= 30 :
+    elif bmi >= 30:
         bmi_str = '中度肥胖'
-    elif bmi >= 27 :
+    elif bmi >= 27:
         bmi_str = '輕度肥胖'
-    elif bmi >= 24 :
+    elif bmi >= 24:
         bmi_str = '過重'
-    elif bmi >= 18.5 :
+    elif bmi >= 18.5:
         bmi_str = '正常範圍'
-    else :
-        bmi_str = '過輕' 
+    else:
+        bmi_str = '過輕'
     return bmi_str
 
-   
-def BMI_math(height_cm:float, weight_kg:float)->tuple[float, str]:
+
+def BMI_math(height_cm: float, weight_kg: float) -> tuple[float, str]:
     height_m = round(height_cm/100, 2)
     bmi_kg_m2 = round(weight_kg/(height_m**2), 2)
     bmi_str = get_status(bmi_kg_m2)
