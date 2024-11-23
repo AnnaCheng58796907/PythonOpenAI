ab9eb30bfb3407b22526fe34194350a95f14a295
鄭筑予 <154043164+AnnaCheng58796907@users.noreply.github.com>
2024-11-23 08:08:47 +0000

    lesson4_14

---
+++ lesson4/lesson4_5.py
@@ -0,0 +1,5 @@
+import tools
+
+s1:tools.Student = tools.Student('小張', age=17, score=245)
+s1.score#property在執行時不用()
+s1.echo()
--- lesson4/tools.py
+++ lesson4/tools.py
@@ -2,7 +2,32 @@ PI = 3.1415962
 
 
 class Person:
-    pass
+    def __init__(self, name:str, age:int):
+        #建立attribute
+        self.name = name
+        self.age = age
+
+
+    #實體方法(instance method)
+    def echo(self):
+        print(f'我的姓名是{self.name}')
+        print(f'我的年齡是{self.age}')
+
+
+class Student(Person):
+    def __init__(self,name:str, age:int, score:int):
+        super().__init__(name=name, age=age)#呼叫父Person的__init__
+        self.__score = score
+
+    
+    #property在執行時不用()
+    @property
+    def score(self)->int:
+        return self.__score
+
+    def echo(self):
+        super().echo()
+        print(f'我的分數是{self.score}')
 
 
 def get_status(bmi: float) -> str:
