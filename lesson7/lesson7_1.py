189d08fc54068cac92bed80adb8058402f97989b
鄭筑予 <154043164+AnnaCheng58796907@users.noreply.github.com>
2024-12-14 06:07:10 +0000

    lesson7_1_3

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,27 +1,4 @@
-import requests
-from requests import Response, RequestException, HTTPError
-from io import StringIO
-from csv import DictReader
+import tools
 
-url = 'https://data.tpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
-
-try:
-
-    r:Response = requests.request('GET', url)
-    r.raise_for_status()
-    print('下載成功')
-    file:StringIO = StringIO(r.text)
-    reader:DictReader = DictReader(file)
-    list_reader:list[dict] = list(reader)
-    for row in list_reader:
-        print(row)        
-
-except HTTPError as e:
-    print(e)
-
-except RequestException as e:
-    print(e)
-
-except Exception as e:
-    print(e)
-
+youbike_data:list[dict] = tools.get_youbikes()
+print(youbike_data)
