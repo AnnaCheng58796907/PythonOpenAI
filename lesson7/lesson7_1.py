70ed8c3ddb081c521a16e9a9f7ae341e56393bf5
鄭筑予 <154043164+AnnaCheng58796907@users.noreply.github.com>
2024-12-14 05:47:18 +0000

    lesson7_1_2

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,23 +1,27 @@
 import requests
-from requests import Response
+from requests import Response, RequestException, HTTPError
 from io import StringIO
 from csv import DictReader
 
-url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
+url = 'https://data.tpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
 
 try:
-    
+
     r:Response = requests.request('GET', url)
-    if r.status_code == 200:
-        print('下載成功')
-        file:StringIO = StringIO(r.text)
-        reader:DictReader = DictReader(file)
-        list_reader:list[dict] = list(reader)
-        for row in list_reader:
-            print(row)        
+    r.raise_for_status()
+    print('下載成功')
+    file:StringIO = StringIO(r.text)
+    reader:DictReader = DictReader(file)
+    list_reader:list[dict] = list(reader)
+    for row in list_reader:
+        print(row)        
 
-    else:
-        print('下載有問題')
+except HTTPError as e:
+    print(e)
 
-except Exception:
-    print('連線有問題')
+except RequestException as e:
+    print(e)
+
+except Exception as e:
+    print(e)
+
