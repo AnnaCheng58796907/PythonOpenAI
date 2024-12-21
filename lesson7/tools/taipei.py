73cc24280617efb2a4948284e6d44f401b5df21d
鄭筑予 <154043164+AnnaCheng58796907@users.noreply.github.com>
2024-12-21 03:02:51 +0000

    lesson8_2

--- lesson7/tools/taipei.py
+++ lesson7/tools/taipei.py
@@ -6,26 +6,21 @@ from csv import DictReader
 import streamlit as st
 
 @st.cache_data
-def get_youbikes()->list[dict]:
-
+def get_youbikes() -> list[dict]:
     url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
 
     try:
-
-        r:Response = requests.request('GET', url)
+        r: Response = requests.request('GET', url)
         r.raise_for_status()
-        
-
-    except HTTPError:
-        raise Exception("伺服器有問題!")
-    except RequestException:
-        raise Exception("連線有問題")
-    except Exception:
-        raise Exception("其他問題")
-
+    except HTTPError as e:
+        raise Exception(f"伺服器有問題: {e}")
+    except RequestException as e:
+        raise Exception(f"連線有問題: {e}")
+    except Exception as e:
+        raise Exception(f"其他問題: {e}")
     else:
-        print('下載成功')
-        file:StringIO = StringIO(r.text)
-        reader:DictReader = DictReader(file)
-        list_reader:list[dict] = list(reader)
-        return list_reader     
+        st.success('下載成功')
+        file: StringIO = StringIO(r.text)
+        reader: DictReader = DictReader(file)
+        list_reader: list[dict] = list(reader)
+        return list_reader
--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 #手動建立counter_key, 並設定初始值為0
 if 'counter' not in st.session_state:
-    st.session_state['counter'] = 0
+    #st.session_state['counter'] = 0
+    st.session_state.counter = 0
 
 
 st.header(f"這網頁已經執行{st.session_state.counter} 次")
